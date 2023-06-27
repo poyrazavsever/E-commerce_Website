f6b348ff714ccaef32e485d796f33c68060b7667
Poyraz Avsever <poyrazavsever@hotmail.com>
2023-06-27 11:28:37 +0300

    feat: add data info

--- productData.jsx
+++ productData.jsx
@@ -3,6 +3,7 @@ export const data = [
         name: 'Dokulu Oversize Gömlek Beyaz 1',
         discount:"600TL",
         price: "188 TL",
+        color:"Beyaz",
         images: [
             {
                 image:"/Product/product1.jpg",
@@ -20,6 +21,20 @@ export const data = [
                 image:"/Product/product1.jpg",
             },
             {
+                image:"/Product/product1.jpg",
+            },
+        ],
+        otherColors: [
+            {
+                color:"Bordo",
+                image:"/Product/product1.jpg",
+            },
+            {
+                color:"Siyah",
+                image:"/Product/product1.jpg",
+            },
+            {
+                color:"Indigo",
                 image:"/Product/product1.jpg",
             },
         ]
@@ -47,6 +62,20 @@ export const data = [
             {
                 image:"/Product/product1.jpg",
             },
+        ],
+        otherColors: [
+            {
+                color:"Bordo",
+                image:"/Product/product1.jpg",
+            },
+            {
+                color:"Siyah",
+                image:"/Product/product1.jpg",
+            },
+            {
+                color:"Indigo",
+                image:"/Product/product1.jpg",
+            },
         ]
     },
     {
@@ -72,343 +101,18 @@ export const data = [
             {
                 image:"/Product/product1.jpg",
             },
-        ]
-    },
-    {
-        name: 'Dokulu Oversize Gömlek Beyaz 4',
-        price: "183 TL",
-        images: [
+        ],
+        otherColors: [
             {
+                color:"Bordo",
                 image:"/Product/product1.jpg",
             },
             {
+                color:"Siyah",
                 image:"/Product/product1.jpg",
             },
             {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-        ]
-    },
-    {
-        discount:"600TL",
-        name: 'Dokulu Oversize Gömlek Beyaz 5',
-        price: "182 TL",
-        images: [
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-        ]
-    },
-    {
-        name: 'Dokulu Oversize Gömlek Beyaz 6',
-        price: "187 TL",
-        images: [
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-        ]
-    },
-    {
-        name: 'Dokulu Oversize Gömlek Beyaz 7',
-        price: "184 TL",
-        images: [
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-        ]
-    },
-    {
-        name: 'Dokulu Oversize Gömlek Beyaz 8',
-        price: "181 TL",
-        images: [
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-        ]
-    },
-    {
-        name: 'Dokulu Oversize Gömlek Beyaz 9',
-        price: "188 TL",
-        images: [
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-        ]
-    },
-    {
-        name: 'Dokulu Oversize Gömlek Beyaz 10',
-        price: "189 TL",
-        images: [
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-        ]
-    },
-    {
-        name: 'Dokulu Oversize Gömlek Beyaz 11',
-        price: "181 TL",
-        images: [
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-        ]
-    },
-    {
-        name: 'Dokulu Oversize Gömlek Beyaz 12',
-        price: "185 TL",
-        images: [
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-        ]
-    },
-    {
-        name: 'Dokulu Oversize Gömlek Beyaz 13',
-        discount:"600TL",
-        price: "158 TL",
-        images: [
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-        ]
-    },
-    {
-        name: 'Dokulu Oversize Gömlek Beyaz 14',
-        price: "148 TL",
-        images: [
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-        ]
-    },
-    {
-        name: 'Dokulu Oversize Gömlek Beyaz 15',
-        price: "198 TL",
-        images: [
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-        ]
-    },
-    {
-        name: 'Dokulu Oversize Gömlek Beyaz 16',
-        price: "288 TL",
-        images: [
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-        ]
-    },
-    {
-        name: 'Dokulu Oversize Gömlek Beyaz 17',
-        discount:"600TL",
-        price: "488 TL",
-        images: [
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
-                image:"/Product/product1.jpg",
-            },
-            {
+                color:"Indigo",
                 image:"/Product/product1.jpg",
             },
         ]

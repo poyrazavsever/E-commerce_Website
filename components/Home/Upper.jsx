b53f28db9ff40d3be9c501dff1d63179596bc55e
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-07-25 18:48:11 +0300

    fix: less code

--- components/Home/Upper.jsx
+++ components/Home/Upper.jsx
@@ -1,20 +1,22 @@
 import Image from "next/image"
+import Link from "next/link"
 
 function Upper() {
+  const imageStyle = "group group-hover:scale-110 shadow-xl group-hover:shadow-neutral-700 transition-all"
   return (
     <div className="flex flex-wrap items-center justify-center gap-8 md:gap-0 md:justify-between">
 
-        <button className="group">
-            <Image src="/Images/main1.jpg" alt="mainPage" width={250} height={200} className="gro group-hover:scale-110 shadow-xl group-hover:shadow-neutral-700 transition-all"/>
-        </button>
+        <Link href="/man" className="group">
+            <Image src="/Images/main1.jpg" alt="mainPage" width={250} height={200} className={imageStyle}/>
+        </Link>
 
-        <button className="group">
-            <Image src="/Images/main2.jpg" alt="mainPage" width={250} height={200} className="gro group-hover:scale-110 shadow-xl group-hover:shadow-neutral-700 transition-all"/>
-        </button>
+        <Link href="/woman" className="group">
+            <Image src="/Images/main2.jpg" alt="mainPage" width={250} height={200} className={imageStyle}/>
+        </Link>
 
-        <button className="group">
-            <Image src="/Images/main3.jpg" alt="mainPage" width={250} height={200} className="gro group-hover:scale-110 shadow-xl group-hover:shadow-neutral-700 transition-all"/>
-        </button>
+        <Link href="/unisex" className="group">
+            <Image src="/Images/main3.jpg" alt="mainPage" width={250} height={200} className={imageStyle}/>
+        </Link>
 
     </div>
   )
